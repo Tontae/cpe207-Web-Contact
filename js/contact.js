3be71a34467c4885ac181a8ad8af641a255f107b
USER <EMAIL>
2019-03-31 16:12:10 +0700

    4 contact

--- js/contact.js
+++ js/contact.js
@@ -80,8 +80,8 @@ function fetchBookmarks() {
         + `<div class="card-header"><h5 class="my-0">${name}</h5></div>`
         + `<div class="card-body" style="width: 200px">`
         + `<div><p>${message}</p></div><br>`
-        + `<a class="primary_btn" target="_blank" href="${url}">Visit</a>`
-        + ` <a onclick="deleteBookmark('${url}')" class="primary_btn tr-bg" href="#">Delete</a>`
+        + `<a class="primary_btn" target="_blank" href="${url}"><span>Visit</span></a>`
+        + ` <a onclick="deleteBookmark('${url}')" class="primary_btn tr-bg" href="#"><span>Delete</span></a>`
         + `</div></div></div>`;
   }
   str += '</div>'
