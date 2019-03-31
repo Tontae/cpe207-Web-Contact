ea96553413933199287028842eddcf59b999556d
USER <EMAIL>
2019-03-31 14:52:32 +0700

    1

--- js/contact.js
+++ js/contact.js
@@ -1,85 +1,113 @@
-$(document).ready(function(){
-    
-    (function($) {
-        "use strict";
+document.querySelector('#contactForm').addEventListener('submit', saveBookmark);
 
-    
-    jQuery.validator.addMethod('answercheck', function (value, element) {
-        return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+function saveBookmark(e) {
+  // prevent default form submitting
+  e.preventDefault();
 
-    // validate contactForm form
-    $(function() {
-        $('#contactForm').validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                subject: {
-                    required: true,
-                    minlength: 4
-                },
-                number: {
-                    required: true,
-                    minlength: 5
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
-                }
-            },
-            messages: {
-                name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
-                },
-                number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
-            }
-        })
-    })
-        
- })(jQuery)
-})
+  // get form values
+  var siteName = document.querySelector('#name').value;
+  var siteEmail = document.querySelector('#email').value;
+  var siteURL = document.querySelector('#url').value;
+  var sitemsg = document.querySelector('#message').value;
+  
+  if (!validateForm(siteName, siteURL, siteEmail)) {
+    return false;
+  }
+
+  var bookmark = {
+    name: siteName,
+    url: siteURL,
+    mail: siteEmail,
+    message: sitemsg
+  }
+
+  console.log(bookmark);
+
+  // // local storage test
+  //   localStorage.setItem('bookmarks',JSON.stringify(bookmark));
+  //   console.log(JSON.parse(localStorage.getItem('bookmarks')));
+
+  if (localStorage.getItem('bookmarks') === null) {
+    var bookmarks = [];
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+  
+  // Clear fields
+  document.querySelector('#name').value = '';
+  document.querySelector('#email').value = '';
+  document.querySelector('#url').value = '';
+  document.querySelector('#message').value = '';
+
+  fetchBookmarks();  
+}
+
+function deleteBookmark(url) {
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  for (var i=0; i<bookmarks.length; i++) {
+    if (bookmarks[i].url === url) {
+      bookmarks.splice(i,1);
+    }
+  }
+
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  fetchBookmarks();
+}
+
+function fetchBookmarks() {
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  var bookmarksResults = document.querySelector('#bookmarksResults');
+
+  bookmarksResults.innerHTML = '';
+
+  var str = '<div class="card-deck text-center">';
+  
+  for (var i=0; i<bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+    var mail = bookmarks[i].mail;
+    var message = bookmarks[i].message;
+
+    str += '<div class="col-sm-6 col-md-4 col-lg-3">' 
+        + `<div class="card mb-4 shadow-sm">`
+        + `<div class="card-header"><h5 class="my-0">${name}</h5></div>`
+        + `<div class="card-body" style="width: 200px">`
+        + `<div><p>${message}</p></div><br>`
+        + `<a class="btn btn-primary" target="_blank" href="${url}">Visit</a>`
+        + ` <a onclick="deleteBookmark('${url}')" class="btn btn-danger" href="#">Delete</a>`
+        + `</div></div></div>`;
+  }
+  str += '</div>'
+
+  bookmarksResults.innerHTML = str;
+}
+
+function validateForm(siteName, siteURL, siteEmail) {
+  if (!siteName || !siteURL || !siteEmail) {
+    alert('Please fill in all data fields');
+    return false;
+  }
+
+  var expURL = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var expEMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var regexURL = new RegExp(expURL);
+  var regexEMAIL = new RegExp(expEMAIL);
+
+  if (!siteURL.match(regexURL)) {
+    alert('Please a valid URL');
+    return false;
+  }
+  if (!siteEmail.match(regexEMAIL)) {
+    alert('Please a valid Email');
+    return false;
+  }
+
+  return true;
+}
